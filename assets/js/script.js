c5c562656cae9ed8b6ee375feb6ff2e02a54691b
sallaball <sallaball1206@gmail.com>
2022-06-25 07:15:32 -0500

    updated textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -80,6 +80,46 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+  .text()
+  .trim();
+
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus"); 
+  
+
+});
+  
+  $(".list-group").on("blur", "textarea", function() {
+var text = $(this).val();
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  $(this).replaceWith(taskP);
+  
+  
 });
 
 // remove all tasks
